49e283eb89262ef3cd87fdc5afbf368493c12a4a
Numan Anwer <anwernuman@gmail.com>
2022-08-04 12:05:22 +0100

    testcommit

---
+++ FilterableProductTable.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+cb dhjvjdvcdvhb
+import ProductTable from "./ProductTable";
+import SearchBar from "./SearchBar";
+
+import { PRODUCTS } from "./products";
+
+const FilterableProductTable = props => {
+  const [filterText, setFilterText] = useState(``);
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  const handleFilterTextChange = changedFilterText => {
+    setFilterText(changedFilterText);
+  };
+
+  const handleInStockOnlyChange = changedInStockOnly => {
+    setInStockOnly(changedInStockOnly);
+  };
+
+  return (
+    <div>
+      <SearchBar
+        searchDetails={{ filterText, inStockOnly }}
+        handleFilterTextChange={handleFilterTextChange}
+        handleInStockOnlyChange={handleInStockOnlyChange}
+      />
+      <ProductTable
+        products={PRODUCTS}
+        searchDetails={{ filterText, inStockOnly }}
+      />
+    </div>
+  );
+};
+
+export default FilterableProductTable;
